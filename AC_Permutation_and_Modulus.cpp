fb5d182ba79895d1c000ae7e478378c36e5d6d4d
Md: Sabbir Hossain Sourav <mdsabbirhossainsourav15@gmail.com>
2026-02-26 15:57:44 +0600

    Add files via upload

---
+++ AC_Permutation_and_Modulus.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        int n;
+        cin >> n;
+        for (int i=1; i<=n; i++) 
+        {
+            if (i != n) 
+            {
+                cout << i + 1 << " ";
+            }
+            else
+            {
+                cout << 1 << endl; 
+            } 
+        }
+    }
+    return 0;
+}
